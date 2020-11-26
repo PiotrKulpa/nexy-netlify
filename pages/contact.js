0540852e134ba22cc8d96578c254603b30e6f677
unknown <info.kulpa@gmail.com>
2020-11-26 17:57:49 +0100

    test server rendering on netlify

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({ testData}) => {
     return (
         <div style={{margin: '55px'}}>
-            <p>Contact</p>
+            <p>Contact { testData }</p>
             <form name="contact" method="POST" data-netlify="true">
                 <p>
-                    <label>Your Nameee: <input type="text" name="name" /></label>
+                    <label>Your Nameee: { testData }<input type="text" name="name" /></label>
                 </p>
                 <p>
                     <label>Your Email: <input type="email" name="email" /></label>
@@ -26,6 +26,14 @@ const Contact = () => {
             </form>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      testData: 'Data served by server side'
+    }, // will be passed to the page component as props
+  }
 }
 
 export default Contact;
